bf12f802ecb9e820db3394d982c937d3c8dba3b3
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-29 13:35:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.0.2@326920 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/gr.php
@@ -0,0 +1,17 @@
+<?php
+    $dir = "ltr";
+    $hor = "right";
+    $dictionary = array(
+        "rateAwful" => "Schrecklich",
+        "ratePoor" => "Bad",
+        "rateAverage" => "Durchschnittliche",
+        "rateGood" => "Gute",
+        "rateExcellent" => "Ausgezeichnet",
+        "rateThis" => "Bewerten Sie diesen",
+        "like" => "Wie",
+        "dislike" => "Abneigung",
+        "vote" => "Abstimmen",
+        "votes" => "Stimmen",
+        "thanks" => "Vielen Dank",
+    );
+?>
---
+++ languages/th.php
@@ -0,0 +1,17 @@
+<?php
+    $dir = "ltr";
+    $hor = "right";
+    $dictionary = array(
+        "rateAwful" => "เหลือร้าย",
+        "ratePoor" => "เลว",
+        "rateAverage" => "เฉลี่ย",
+        "rateGood" => "ดี",
+        "rateExcellent" => "ยอดเยี่ยม",
+        "rateThis" => "ให้คะแนนเว็บนี้",
+        "like" => "ชอบ",
+        "dislike" => "ไม่ชอบ",
+        "vote" => "เลือกตั้ง",
+        "votes" => "จำนวนผู้ลงคะแนน",
+        "thanks" => "ขอบคุณ",
+    );
+?>
