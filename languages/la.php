e799b2196d977bd913f711a6a759947c724d7353
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-30 13:04:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.0.3@327168 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/la.php
@@ -0,0 +1,17 @@
+<?php
+    $dir = "ltr";
+    $hor = "right";
+    $dictionary = array(
+        "rateAwful" => "Awful",
+        "ratePoor" => "Poor",
+        "rateAverage" => "Average",
+        "rateGood" => "Bonum",
+        "rateExcellent" => "Excellent",
+        "rateThis" => "Rate hoc",
+        "like" => "Like",
+        "dislike" => "ODI",
+        "vote" => "Vote",
+        "votes" => "Votes",
+        "thanks" => "Gratias",
+    );
+?>
