0ff28264e5c86b76757643aec85999bb576e6804
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-05 01:22:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.0.7@328848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/ru.php
@@ -0,0 +1,17 @@
+<?php
+    $dir = "ltr";
+    $hor = "right";
+    $dictionary = array(
+        "rateAwful" => "Ужасный",
+        "ratePoor" => "Плохо",
+        "rateAverage" => "Среднее",
+        "rateGood" => "Хорошее",
+        "rateExcellent" => "Отлично",
+        "rateThis" => "Проголосовать этом",
+        "like" => "Нравится",
+        "dislike" => "Не нравится",
+        "vote" => "Голос",
+        "votes" => "Голосов",
+        "thanks" => "Спасибо",
+    );
+?>
