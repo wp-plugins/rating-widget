f4cbafaffdd9ca6f06a1bcc71b73ae2c19c45558
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-26 19:48:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.2.0@337570 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view/options.php
@@ -0,0 +1,17 @@
+<div class="has-sidebar has-right-sidebar">
+    <div class="has-sidebar-content">
+        <div class="postbox rw-body" style="width: 610px;">
+            <h3>Rating-Widget Options</h3>
+            <div class="inside rw-ui-content-container rw-no-radius">
+                <table>
+                    <?php include_once($this->base_dir . "/view/settings/language.php");?>
+                    <?php include_once($this->base_dir . "/view/settings/type.php");?>
+                    <?php include_once($this->base_dir . "/view/settings/theme.php");?>
+                    <?php include_once($this->base_dir . "/view/settings/size.php");?>
+                    <?php include_once($this->base_dir . "/view/settings/read_only.php");?>
+                </table>
+                <?php include_once($this->base_dir . "/view/settings/advanced.php");?>
+            </div>
+        </div>
+    </div>
+</div>
