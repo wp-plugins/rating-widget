150842285f74e04d3f9ce70936ee805546d49f4c
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-01 01:32:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.2.4@339425 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/dir.php
@@ -0,0 +1,22 @@
+<?php
+    $rw_languages = array(
+//        "af" => "Afrikaans",
+        "ar" => "Arabic - العربية",
+//        "ch" => "Chinese",
+        "hr" => "Croatian - Hrvatski", 
+        "en" => "English (Default)", 
+        "fr" => "French - Français",
+        "de" => "German - Deutsch",
+        "he" => "Hebrew - עברית",
+//        "hi" => "Hindi",
+//        "it" => "Italian",
+//        "jp" => "Japanese",
+//        "ko" => "Korean",
+//        "la" => "Latin",
+        "pl" => "Polish - Polska",
+        "pt" => "Portuguese - Português",
+        "ru" => "Russian - Русский",
+        "es" => "Spanish - Español",
+//        "th" => "Thai",
+    );
+?>
