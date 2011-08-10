da6272236c87c133826ce8807c03cb72b19b20b1
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-10 16:21:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/tags/1.3.5@421696 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rating-widget.php
+++ rating-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Rating-Widget Plugin
 Plugin URI: http://rating-widget.com
 Description: Create and manage Rating-Widget ratings in WordPress.
-Version: 1.3.4
+Version: 1.3.5
 Author: Vova Feldman
 Author URI: http://il.linkedin.com/in/vovafeldman
 License: A "Slug" license name e.g. GPL2
