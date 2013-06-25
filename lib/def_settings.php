d61c47b59bfe9307dd210ac78affacfdc2b2011d
svovaf <svovaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-25 07:52:30 +0000

    = 1.8.2 = * Fix: Ratings control in admin's dashboard.
    git-svn-id: https://plugins.svn.wordpress.org/rating-widget/trunk@731392 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/def_settings.php
+++ lib/def_settings.php
@@ -49,17 +49,15 @@
             }
         }
         
-        $hasTheme = $loadTheme && isset($settings->theme);
-
-        require_once(RW__PATH_THEMES . "dir.php");
-        
-        global $RW_THEMES;
-        
         // Get rating type.
-        $ret->type = @rw_get_default_value($settings->type, (!$hasTheme ? $defaults->type : ($RW_THEMES['star'][$settings->theme] ? 'star' : 'nero')));
+        $ret->type = @rw_get_default_value($settings->type, $defaults->type);
 
-        if ($hasTheme)
+        if ($loadTheme && isset($settings->theme))
         {
+            require_once(RW__PATH_THEMES . "dir.php");
+            
+            global $RW_THEMES;
+            
             // Load theme options.
             require(RW__PATH_THEMES . $RW_THEMES[$ret->type][$settings->theme]["file"]);
             
@@ -159,9 +157,9 @@
         $settings->advanced->text = @rw_get_default_value($settings->advanced->text, new stdClass());
         
         $settings->lng = @rw_get_default_value($settings->lng, "en");
-        $settings->url = @rw_get_default_value($settings->url, '');
-        $settings->img = @rw_get_default_value($settings->img, '');
-        $settings->title = @rw_get_default_value($settings->title, '');
+        $settings->url = @rw_get_default_value($settings->url, "");
+        $settings->img = @rw_get_default_value($settings->img, "");
+        $settings->title = @rw_get_default_value($settings->title, "");
         $settings->type = @rw_get_default_value($settings->type, $type);
         $settings->rclass = @rw_get_default_value($settings->rclass, "");
         $settings->size = @rw_get_default_value($settings->size, "small");
